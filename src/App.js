06b710fa9a4705385436eb10b3c0103c69c325cc
Lindsey Martin <49573760+Lemming97@users.noreply.github.com>
2022-08-05 12:40:47 -0700

    added footer and updated the app.js file

--- src/App.js
+++ src/App.js
@@ -1,31 +1,36 @@
-import React, { useState } from 'react';
-import Header from '../src/components/Header';
-import About from '../src/components/About';
-import './App.css';
+import React, { useState } from "react";
+import Header from "../src/components/Header";
+import About from "../src/components/About";
+import Footer from "../src/components/Footer";
+import "./App.css";
 
 function App() {
   const [currentTab, handleTabChange] = useState("about");
 
+  // This method is checking to see what the value of `currentTab` is. Depending on the value of currentPage, we return the corresponding component to render.
+
   const renderTab = () => {
-    switch (currentTab) {
-      case "about":
-        return <About />;
-      default:
-        return null;
+    if (currentTab === "About") {
+      return <About />;
     }
   };
   return (
     <div>
-			<div>
-				<Header currentTab={currentTab} handleTabChange={handleTabChange}></Header>
-			</div>
-			<div>
-				<main>{renderTab()}</main>
-			</div>
-      {/* <div>
-        <Footer></Footer>
-      </div> */}
-		</div>
+      <div>
+        <Header
+          currentTab={currentTab}
+          handleTabChange={handleTabChange}
+        ></Header>
+      </div>
+      <div>
+        <main>{renderTab()}</main>
+      </div>
+      <div>
+        <footer>
+          <Footer></Footer>
+        </footer>
+      </div>
+    </div>
   );
 }
 
