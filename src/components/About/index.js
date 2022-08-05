c1ac7019afa112dbf98584d7d20431513c66af76
Lindsey Martin <49573760+Lemming97@users.noreply.github.com>
2022-08-05 12:13:29 -0700

    Updated about me section and added styling.

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,22 +1,37 @@
 import React from "react";
-// import coverImage from '../../assets/cover/5330558.jpg';
+import profileImage from "../../assets/large/Profile/Profile_Pic.png";
 function About() {
   return (
-    <section className="my-5">
-      <h1 id="about">Who am I?</h1>
-      {/* <img src={coverImage} className="my-2" style={{ width: "100%" }} alt="cover" /> */}
-      <div className="my-2">
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
-          bibendum turpis sed ex condimentum molestie. Mauris condimentum lectus
-          ut ornare dignissim. Mauris faucibus urna mi, ac feugiat metus aliquam
-          maximus. Proin aliquam justo nec diam vulputate vestibulum. Aenean
-          sollicitudin nulla at nisi ornare, nec suscipit massa eleifend. Morbi
-          tristique justo vel turpis sollicitudin, et tristique velit convallis.
-          In hac habitasse platea dictumst. Phasellus mattis nunc sed orci
-          consequat laoreet. Praesent id nisl nibh. Curabitur imperdiet
-          ultricies mollis. In hac habitasse platea dictumst.
-        </p>
+    <section id="about-me" className="my-5 intro">
+      <div class="flex-row">
+        <h1 id="about" className="section-title primary-border">
+          About Me
+        </h1>
+      </div>
+      <div className="flex-row">
+        <div class="intro-info">
+          <div class="intro-img">
+            <img
+              src={profileImage}
+              style={{ width: "100%" }}
+              alt="profile"
+            />
+          </div>
+          <p>
+            Hello, my name is Lindsey Martin. I have currently 30 living in Los
+            Angeles. I have a B.A. in Psychology with a concentration in
+            Neuroscience from the University of San Francisco, and graduated in
+            2016. I moved up to San Francisco for college and ended up staying
+            in the city after college. I was living in the city really until
+            COVID, but when the city shut down I started going back and forth
+            between Los Angeles and San Francisco. My family is in Southern
+            California, so it was nice to be able to spend time with them. When
+            I'm not working, I like to spend my time being active. I like going
+            for long walks with my dogs as well as small hikes with friends. I
+            love to exercise and try to do some form of exercise daily that can
+            range from pilates, weight training, yoga, or a hot HIIT class.
+          </p>
+        </div>
       </div>
     </section>
   );
