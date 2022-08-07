5958bfae42c8bde90335ac76662a598921d55b88
Lindsey Martin <49573760+Lemming97@users.noreply.github.com>
2022-08-06 20:31:22 -0700

    Added starter templates for contact resume and portfolio page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { useState } from "react";
 import Header from "../src/components/Header";
 import About from "../src/components/About";
+import Contact from "../src/components/Contact";
+import Portfolio from "../src/components/Portfolio";
+import Resume from "../src/components/Resume";
 import Footer from "../src/components/Footer";
 import "./App.css";
 
@@ -8,12 +11,22 @@ function App() {
   const [currentTab, handleTabChange] = useState("about");
 
   // This method is checking to see what the value of `currentTab` is. Depending on the value of currentPage, we return the corresponding component to render.
-
   const renderTab = () => {
     if (currentTab === "About") {
       return <About />;
     }
+    if (currentTab === "Contact") {
+      return <Contact />;
+    }
+    if (currentTab === "Portfolio") {
+      return <Portfolio />;
+    }
+    if (currentTab === "Resume") {
+      return <Resume />;
+    }
+    return <About />;
   };
+
   return (
     <div>
       <div>
