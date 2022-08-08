77c6766c2b6bbc759a52afc201b42c4ee52d6267
Lindsey Martin <49573760+Lemming97@users.noreply.github.com>
2022-08-08 13:26:44 -0700

    removed padding in smaller screen

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,23 +1,25 @@
 import React from "react";
 
 function Footer() {
-  <div>
-    <footer>
-      <h2>Made by Lindsey Martin©</h2>
-      <p>
-        Stock images are from
-        <a href="https://unsplash.com/" target="blank" rel="test">
-          <li className="logo">{/* <FaLinkedin /> */}Unsplash</li>
-        </a>
-      </p>
-      <p>
-        Assets are from
-        <a href="https://www.freepik.com/" target="blank" rel="test">
-          <li className="logo">{/* <FaLinkedin /> */}Freepik</li>
-        </a>
-      </p>
-    </footer>
-  </div>;
+  return (
+    <div>
+      <footer>
+        <h2>Made by Lindsey Martin©</h2>
+        <p>
+          Stock images are from
+          <a href="https://unsplash.com/" target="blank" rel="test">
+            <li className="logo">{/* <FaLinkedin /> */}Unsplash</li>
+          </a>
+        </p>
+        <p>
+          Assets are from
+          <a href="https://www.freepik.com/" target="blank" rel="test">
+            <li className="logo">{/* <FaLinkedin /> */}Freepik</li>
+          </a>
+        </p>
+      </footer>
+    </div>
+  );
 }
 
 export default Footer;
