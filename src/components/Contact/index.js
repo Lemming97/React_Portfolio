3ac2c9f61b43ff4fc1264167a7d98d469fcefe1e
Lindsey Martin <49573760+Lemming97@users.noreply.github.com>
2022-08-08 15:24:28 -0700

    Added checks for contact form

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,64 +1,89 @@
-import React from "react";
+import React, { useState } from "react";
+
+// Here we import a helper function that will check if the email is valid
+import { validateEmail } from "../../utils/helpers";
 
 function Contact() {
-  //need to include validateEmail from helpers function
+  // Create state variables for the fields in the form
+  // We are also setting their initial values to an empty string
+  const [email, setEmail] = useState("");
+  const [userName, setUserName] = useState("");
+  const [message, setMessage] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
 
-  //rerender the page
+  const handleInputChange = (e) => {
+    // Getting the value and name of the input which triggered the change
+    const { target } = e;
+    const inputType = target.name;
+    const inputValue = target.value;
+
+    // Based on the input type, we set the state of either email, username, and message
+    if (inputType === "email") {
+      setEmail(inputValue);
+    } else if (inputType === "userName") {
+      setUserName(inputValue);
+    } else {
+      setMessage(inputValue);
+    }
+  };
+
+  const handleFormSubmit = (e) => {
+    // Preventing the default behavior of the form submit (which is to refresh the page)
+    e.preventDefault();
+
+    // First we check to see if the email is not valid or if the userName is empty. If so we set an error message to be displayed on the page.
+    if (!validateEmail(email) || !userName) {
+      setErrorMessage("Email or Name is invalid");
+      // We want to exit out of this code block if something is wrong so that the user can correct it
+      return;
+      // Then we check to see if the message is not valid. If so, we set an error message regarding the message.
+    }
+
+    if (!setMessage(message)) {
+      setErrorMessage(`Message is required.`);
+      return;
+    }
+
+    // If everything goes according to plan, we want to clear out the input after a successful submission.
+    setUserName("");
+    setMessage("");
+    setEmail("");
+  };
+
   return (
     <div>
-      <section id="contact-me" className="reach-out">
-        <div className="flex-row">
-          <h2 className="section-title secondary-border">Contact Me</h2>
+      <p>Hello {userName}</p>
+      <form className="form">
+        <input
+          value={email}
+          name="email"
+          onChange={handleInputChange}
+          type="email"
+          placeholder="email"
+        />
+        <input
+          value={userName}
+          name="userName"
+          onChange={handleInputChange}
+          type="text"
+          placeholder="username"
+        />
+        <input
+          value={message}
+          name="message"
+          onChange={handleInputChange}
+          type="message"
+          placeholder="message"
+        />
+        <button type="button" onClick={handleFormSubmit}>
+          Submit
+        </button>
+      </form>
+      {errorMessage && (
+        <div>
+          <p className="error-text">{errorMessage}</p>
         </div>
-        <div className="contact-info">
-          <div>
-            <h3>Feel free to contact me via the following pathways:</h3>
-            <p>
-              Any questions or concerns with your order?
-              <br />
-              Let us know and we'll be happy to talk to you!
-            </p>
-            <address>
-              <ul id="contact-list">
-                <p>
-                  <li>
-                    <a href="https://github.com/Lemming97">Github</a>
-                  </li>
-                  <li>
-                    <a href="https://open.spotify.com/playlist/0A1mKAUFXo0A8h4aRKpGwV?si=9fc3f59a67854c69">
-                      Spotify
-                    </a>
-                  </li>
-                  <li>
-                    {" "}
-                    Phone: <a href="tel:555.786.2839">(555.786.2839)</a>
-                  </li>
-                  <li>
-                    {" "}
-                    Email:{" "}
-                    <a href="mailto://martinlindsey87@yahoo.com">
-                      Martinlindsey87@yahoo.com
-                    </a>
-                  </li>
-                </p>
-              </ul>
-            </address>
-          </div>
-        </div>
-
-        <div className="contact-form">
-          <h3>Contact Us</h3>
-          <form>
-            <label for="contact-name">Your Name</label>
-            <input type="text" id="contact-name" placeholder="Your Name" />
-
-            <label for="contact-message">Message</label>
-            <textarea id="contact-message" placeholder="Message"></textarea>
-
-            <button type="submit">Submit</button>
-          </form>
-        </div>
-      </section>
+      )}
     </div>
   );
 }
