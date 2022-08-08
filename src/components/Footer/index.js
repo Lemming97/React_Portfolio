205d18f8a68c4d93af754e6da76e94f39e8cb500
Lindsey Martin <49573760+Lemming97@users.noreply.github.com>
2022-08-08 16:57:44 -0700

    Finished footer with icons

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { VscGithubAlt } from "react-icons/vsc";
 import { AiOutlineLinkedin } from "react-icons/ai";
+import { FaInstagram } from "react-icons/fa";
 
 function Footer() {
   return (
@@ -16,6 +17,11 @@ function Footer() {
           <a href="https://www.linkedin.com/in/lindsey-m-2219abb0/">
             <li className="logo">
               <AiOutlineLinkedin />
+            </li>
+          </a>
+          <a href="https://www.instagram.com/lem_photos/">
+            <li className="logo">
+              <FaInstagram />
             </li>
           </a>
         </ul>
