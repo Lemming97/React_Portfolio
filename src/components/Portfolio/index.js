1c33a7e292358d905217b3ede2a5880a9d34c9e6
Lindsey Martin <49573760+Lemming97@users.noreply.github.com>
2022-08-24 17:08:17 -0700

    added new job

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -5,6 +5,7 @@ import weatherDashImage from "../../assets/small/Weather_Dash.png";
 import noteTakerImage from "../../assets/small/NoteTaker.png";
 import twilightImage from "../../assets/small/Twilight_Ristorante.png";
 import flowerImage from "../../assets/small/Sweet-Pea-Flowers.png";
+import sphereImage from "../../assets/small/Sphere.png";
 
 function Portfolio() {
   return (
@@ -12,6 +13,32 @@ function Portfolio() {
       <section id="work" className="jobs">
         <div className="flex-row">
           <h2 className="section-title secondary-border">Work</h2>
+        </div>
+
+        <div className="job">
+          <div className="job-info">
+            <div className="job-img">
+              <a href="https://lemming97.github.io/Sweet-Pea-Flowers/">
+                {" "}
+                <img
+                  src={sphereImage}
+                  className="my-2"
+                  style={{ width: "100%" }}
+                  alt="Sweet-Pea-Flowers"
+                />
+              </a>
+            </div>
+            <div className="job-text">
+              <h4>Sorcerer's Sphere</h4>
+              <p>
+                A MERN application that lets the user log in and create an
+                account. The user can receive daily affirmations. The user also
+                has the ability to ask questions to receive an answer. The user
+                also can receive a lucky number if they want. The user can
+                donate money to the website if they want to.{" "}
+              </p>
+            </div>
+          </div>
         </div>
 
         <div className="job">
